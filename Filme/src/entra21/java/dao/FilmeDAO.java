3e87c86ece8f59799cacd5447003039d53b37956
MarcioAugustoInkster <gersonsbeltrame@gmail.com>
2018-10-09 11:22:59 -0300

    Continuação

--- Filme/src/entra21/java/dao/FilmeDAO.java
+++ Filme/src/entra21/java/dao/FilmeDAO.java
@@ -27,22 +27,22 @@ public class FilmeDAO {
                 if (rs.next()) {
                     filme = new FilmeBean();
                     
-                    filme.setNome(rs.getString("Nascido para Morrer"));
-                    filme.setDiretor(rs.getString("Edward Lowrence"));
-                    filme.setCategoria(rs.getString("Ação"));
-                    filme.setAno(rs.getShort(1995));
-                    filme.setAtorPrincipal(rs.getString("Richard Crow"));
-                    filme.setTempoFilme(rs.getShort(180));
-                    filme.setOrcamento(rs.getDouble(15000));
-                    filme.setFaturamento(rs.getDouble(25000));
-                    filme.setFaixaEtaria(rs.getShort(18));
-                    filme.setLegenda(true);
-                    filme.setDublado(true);
-                    filme.setIdiomaOriginal(rs.getString("Inglês"));
+                    filme.setNome(rs.getString("nome"));
+                    filme.setDiretor(rs.getString("diretor"));
+                    filme.setCategoria(rs.getString("categoria"));
+                    filme.setAno(rs.getShort("ano"));
+                    filme.setAtorPrincipal(rs.getString("ator_principal"));
+                    filme.setTempoFilme(rs.getShort("tempo_filme"));
+                    filme.setOrcamento(rs.getDouble("orcamento"));
+                    filme.setFaturamento(rs.getDouble("faturamento"));
+                    filme.setFaixaEtaria(rs.getShort("faixa_etaria"));
+                    filme.setLegenda(rs.getBoolean("legenda"));
+                    filme.setDublado(rs.getBoolean("dublado"));
+                    filme.setIdiomaOriginal(rs.getString("idioma_original"));
                     
                     return filme;
                 }
-            } catch (Exception ex) {
+            } catch (SQLException ex) {
                 ex.printStackTrace();
             }
         }
@@ -50,6 +50,8 @@ public class FilmeDAO {
     }
     
     public int inserir(FilmeBean filme) {
+        coneccao = Banco.conectar();
+        
         if (coneccao != null) {
             String sql = "INSERT INTO filmes (nome, diretor, categoria, ano, ";
                 sql += "ator_principal, tempo_filme, orcamento, faturamento, ";
@@ -87,17 +89,56 @@ public class FilmeDAO {
         return -1;
     }
     
-    public boolean excluir() {
+    public boolean excluir(int id) {
+        coneccao = Banco.conectar();
         
-        
+        if (coneccao != null) {
+            String sql = "DELETE FROM filmes WHERE id = ?";
+            
+            try {
+                PreparedStatement ps = coneccao.prepareStatement(sql);
+                ps.setInt(1, id);
+                
+                return ps.executeUpdate() == 1;
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            } finally {
+                Banco.desconectar();
+            }
+        }
         return false;
     }
     
-    public boolean editar() {
+    public boolean editar(FilmeBean filme) {
         if (coneccao != null) {
-            
+            String sql = "UPDATE filmes SET nome=?, diretor=?, categoria=? ";
+                sql += "ano=?, ator_principal=?, tempo_filme=?, orcamento=?, ";
+                sql += "faturamento=?, faixa_etaria=?, legenda=?, dublado=?, ";
+                sql += "idioma_original=? WHERE id = ?";
+                
+            try {
+                PreparedStatement ps = coneccao.prepareStatement(sql);
+                ps.setString(1, filme.getNome());
+                ps.setString(2, filme.getDiretor());
+                ps.setString(3, filme.getCategoria());
+                ps.setShort(4, filme.getAno());
+                ps.setString(5, filme.getAtorPrincipal());
+                ps.setShort(6, filme.getTempoFilme());
+                ps.setDouble(7, filme.getOrcamento());
+                ps.setDouble(8, filme.getFaturamento());
+                ps.setShort(9, filme.getFaixaEtaria());
+                ps.setBoolean(10, filme.isLegenda());
+                ps.setBoolean(11, filme.isDublado());
+                ps.setString(12, filme.getIdiomaOriginal());
+                ps.setInt(13, filme.getId());
+                
+                return ps.executeUpdate() == 1;
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            } finally {
+                Banco.desconectar();
+            }
         }
-        
         return false;
     }
 }
--- Filme/test/dao/FilmeJUnitTest.java
+++ Filme/test/dao/FilmeJUnitTest.java
@@ -1,5 +1,21 @@
 package dao;
 
+import entra21.java.bean.FilmeBean;
+
 public class FilmeJUnitTest {
-    
+    public void testeInserir() {
+        FilmeBean filme = new FilmeBean();
+        filme.setNome("O Programador de Programa");
+        filme.setDiretor("Isaac Newton");
+        filme.setCategoria("Ação, Suspense");
+        filme.setAno((short)1992);
+        filme.setAtorPrincipal("Rocky Balboa");
+        filme.setTempoFilme((short)96);
+        filme.setOrcamento(15000);
+        filme.setFaturamento(5000);
+        filme.setFaixaEtaria((short)18);
+        filme.setLegenda(true);
+        filme.setDublado(true);
+        filme.setIdiomaOriginal("Inglês");
+    }
 }
